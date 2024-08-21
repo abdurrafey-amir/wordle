484723d1fad0127e37581d2108d915f6b6e6cfb8
abdurrafey-amir <abdurrafeyamir15@gmail.com>
2024-08-21 18:49:41 +0500

    inputs and word highlighting

--- main.py
+++ main.py
@@ -23,14 +23,26 @@ board = [
 
 font = pygame.font.Font("freesansbold.ttf", 50)
 
+# colors
+grey = pygame.Color('grey12')
+white = (255, 255, 255)
+green = (0, 255, 0)
+
+
+
+
 def draw_board():
+    global rects
+    rects = [[], [], [], [], []]
+    
     for col in range(5):
         for row in range(6):
-            pygame.draw.rect(screen, (255, 255, 255), (col * 100 + 12, row * 100 + 12, 75, 75), 2)
+            rect = pygame.FRect((col * 100 + 12, row * 100 + 12, 75, 75))
+            pygame.draw.rect(screen, (255, 255, 255), rect, 2)
             text = font.render(board[row][col], True, (255, 255, 255))
             screen.blit(text, (col * 100 + 35, row * 100 + 25))
-            
-
+            rects[col].append(rect)
+    return rects
 
 turn = 0
 
@@ -91,17 +103,27 @@ def letter(event):
         input_letter('z')
     
 
-
+row = 0
 
 def input_letter(letter):
-    global turn
-    for list in board:
-        list[turn] = letter
-        turn += 1
-        if turn == 5:
+    global turn, row, running
+    letter = letter.upper()
+    board[row][turn] = letter
+    turn += 1
+    if turn == 5:
             turn = 0
-        print(list)
-        print(turn)
+            row += 1
+    if row == 6:
+        running = False
+        print('Game Over')
+    # print(turn)
+
+def color(rects):
+    global turn, row
+    rect = rects[turn-1][row]
+    # rect = rects[row]
+    pygame.draw.rect(screen, green, rect, 2)
+    pass
 
 
 # other rects
@@ -109,8 +131,6 @@ info = pygame.Rect(0, HEIGHT - 80, WIDTH, 80)
 info_text = font.render('Wordle', True, (255, 255, 255))
 
 
-# colors
-grey = pygame.Color('grey12')
 
 # main loop
 running = True
@@ -181,13 +201,15 @@ while running:
 
 
     screen.fill(grey)
-    draw_board()
+    rects = draw_board()
     pygame.draw.rect(screen, (255, 255, 255), info, 2)
     # input_letter()
+    color(rects)
     inf = screen.blit(info_text, (WIDTH / 2 - 70, HEIGHT - 60))
-
+    
 
     # update display
     pygame.display.flip()
 
 pygame.quit()
+print(rects)
